9911165d0a1ddbf67773fde4a6805102531fc1a6
Matthew Hollick <22219356+matthew-hollick@users.noreply.github.com>
2023-04-20 09:55:54 +0100

    fleek: apply

--- aliases.nix
+++ aliases.nix
@@ -2,6 +2,8 @@
    home.shellAliases = {
     cat = "bat";
     
+    cdsrc = "cd ~/src/HMRC";
+    
     fleeks = "cd /Users/mjh/.local/share/fleek";
     
     gitsub = "git submodule update --init --recursive";
