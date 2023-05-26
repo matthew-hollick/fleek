c3599d098616597cab551447a10f6d5383061b4f
Matthew Hollick <22219356+matthew-hollick@users.noreply.github.com>
2023-05-26 10:29:32 +0100

    fleek: apply

--- user.nix
+++ user.nix
@@ -5,7 +5,6 @@
   home.file = {
     ".tmux.conf" = {
      text = ''
-     set-option -g default-shell /run/current-system/sw/bin/fish
      set-window-option -g mode-keys vi
      set -g default-terminal "screen-256color"
      set -ga terminal-overrides ',screen-256color:Tc'
